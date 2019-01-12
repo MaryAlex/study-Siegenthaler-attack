39990e2a0805157ee2e77a34c9e3148934b59414
MaryAlex <maria.chmyreva@gmail.com>
2019-01-13 01:24:19 +0300

    Print fixed. Still does not work.

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "attack.h"
 
 #define GIVEN_N 3
-#define SEQUENCE_SIZE 4
+#define SEQUENCE_SIZE 100000
 
 void print_array(uint16_t *array, int size) {
     for (int i = 0; i < size; ++i) {
@@ -15,8 +15,8 @@ void print_array(uint16_t *array, int size) {
 int main() {
     Generator generator = create_generator(GIVEN_N);
     uint8_t *sequence = generate_sequence(generator, SEQUENCE_SIZE);
-    print_array(generator.init_states, SEQUENCE_SIZE);
+    print_array(generator.init_states, GIVEN_N);
     uint16_t *test_sequence = runSiegenthaler(generator.N, SEQUENCE_SIZE, sequence);
-    print_array(test_sequence, SEQUENCE_SIZE);
+    print_array(test_sequence, GIVEN_N);
     return 0;
 }
